193bea0793bdbe4818f5218061ebb14505eac322
Documenter.jl <documenter@juliadocs.github.io>
2022-07-22 18:58:27 +0000

    build based on 06a6d8e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VlasovPoissonTwoSpecies","category":"page"},{"location":"#VlasovPoissonTwoSpecies","page":"Home","title":"VlasovPoissonTwoSpecies","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VlasovPoissonTwoSpecies.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VlasovPoissonTwoSpecies]","category":"page"}]
+}
