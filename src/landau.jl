6038b07d608da07b758c090d15fbd43ec655f5dd
Pierre Navaro <navaro@math.cnrs.fr>
2022-07-25 11:22:28 +0200

    Update landau.jl

--- src/landau.jl
+++ src/landau.jl
@@ -5,14 +5,18 @@ $(SIGNATURES)
 
 # Landau Damping
 
+```math
+f(x,v) = \\frac{1}{\\sqrt{2\\pi}}(1 + \\epsilon \\cos ( k x )) \\exp (- \\frac{v^2}{2} )
+```
+
 [Landau damping - Wikipedia](https://en.wikipedia.org/wiki/Landau_damping)
 """
-function landau(ϵ, kx, meshx, meshv)
+function landau(ϵ, k, meshx, meshv)
     nx = meshx.nx
     nv = meshv.nx
     x = meshx.x
     v = meshv.x
     f = zeros(Float64, (nx, nv))
-    f .= (1.0 .+ ϵ * cos.(kx * x)) / sqrt(2π) .* transpose(exp.(-0.5 * v .^ 2))
+    f .= (1.0 .+ ϵ * cos.(k * x)) / sqrt(2π) .* transpose(exp.(-0.5 * v .^ 2))
     f
 end
